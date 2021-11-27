0a519f823a07306fa59ce154e1580dfe828f3d29
Sven Bolte <67223729+svenbolte@users.noreply.github.com>
2021-11-27 12:35:46 +0100

    statistics, date format improved in lists

--- chartscodes.php
+++ chartscodes.php
@@ -572,7 +572,10 @@ function website_display_stats() {
 				$values.= $customer->pidcount.',';
 				$xsum += absint($customer->pidcount);
 				$html .= '<tr><td>' . $customer->pidcount . '</td><td><a title="Post aufrufen" href="'.get_the_permalink($customer->postid).'">' . get_the_title($customer->postid) . '</a> &nbsp; ';
-				$html .= get_the_date( 'd. F Y', $customer->postid );
+				//$html .= get_the_date( 'd. F Y', $customer->postid );
+				
+				$html .= date_i18n(get_option('date_format') . ' ' . get_option('time_format'), strtotime(get_the_date( 'd. F Y', $customer->postid )) );
+				
 				$html .= sprintf(__(', visitors alltime: %s', 'pb-chartscodes'),get_post_meta( $customer->postid, 'post_views_count', true )) . '</td></tr>';
 			}	
 			$html .= '<tr><td colspan=2>'.sprintf(__('<strong>%s</strong> sum of values', 'pb-chartscodes'),$xsum).' &Oslash; '.number_format_i18n( ($xsum/count($customers)), 2 ).'</td></tr>';
